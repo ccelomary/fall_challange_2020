f987e2983a0c204c42c1b0d14d3742e1bb92dc1b
Mohamed El omary <mel-omar@e2r11p7.1337.ma>
2020-11-16 14:09:57 +0100

    fall_challange

--- test.py
+++ test.py
@@ -50,6 +50,7 @@ def brew(my_data, target):
 def cast(cast_h, my_data):
     for i in range(4):
         my_data[i] += cast_h[i]
+    return my_data
 
 def reset(casted_id):
     casted_id.clear()
@@ -60,27 +61,29 @@ def reset(casted_id):
 """
 
 d = {
-    "".join((str(num) for num in tst1)): tst1
+    "".join((str(num) for num in ingradients[0][:4])): ingradients[0][:4]
 }
-d.update({"".join((str(num) for num in gradient[:4])) : gradient for gradient in casts_tst})
+d.update({"".join((str(num) for num in gradient[:4])) : gradient for gradient in casts})
+
 def check_for_brew(data, gradient):
     if all((data[i] >= abs(gradient[i]) for i in range(4))):
         return True
-
+    return False
 def check_for_cast(data, cast):
     return all((cast[i] + data[i] >= 0 for i in range(4)))
 
-def get_all_child(gradient, casts):
-    name = "".join((str(num) for num in gradient))
+def get_all_child(data, gradient, casts):
+    name = "".join((str(num) for num in gradient[:4]))
     re = {}
     children = []
     for i in range(4):
-        if gradient[i] < 0:
+        if gradient[i] + data[i] < 0:
             for j in range(len(casts)):
                 if casts[j][i] > 0:
-                    children.append(get_all_child(casts[j], casts[:j] + casts[j + 1:]))
-    re[name] = children
-    return re
+                    children.append(get_all_child(data, casts[j], casts[:j] + casts[j + 1:]))
+                    re[name] = children
+                    return re
+    return {name:[]}
 
 """
 def calculate_number_steps(mdata, gradient, casts, l = 1):
@@ -89,21 +92,39 @@ def calculate_number_steps(mdata, gradient, casts, l = 1):
             print('-' * i, 'less', gradient, mdata)
             for k i
 """
-def search_items(data, parent):
+
+def calculate_probability(layers):
+    cast_pro = ("".join((str(num) for num in layer[:4]))  for layer in layers)
+    print(layers)
+    f = [1/ (1 + abs(sum(l[:4]))) for l in layers]
+    t = sum(f)
+    return zip(cast_pro, (num / t for num in f))
+
+def search_items(data, parent, proba):
     queue = [parent]
     while queue:
         node = queue.pop(0)
         key = list(node.keys())[0]
-        print(d[key])
         if check_for_cast(data, d[key]):
             print("found")
-            return key
-        queue += list(node.values())[0]
-
-
+            return d[key]
+        queue += sorted(list(node.values())[0], key=lambda x: -proba[list(x.keys())[0]])
 
 if __name__ == '__main__':
-    print(check_for_brew(my_data, tst1))
-    children = get_all_child(tst1, casts_tst)
-    print(children)
-    search_items(my_data, children)
+    count = 0
+    proba = dict(calculate_probability(casts))
+    print(d)
+    pass
+    while not check_for_brew(my_data, ingradients[0]):
+        parent = get_all_child(my_data, ingradients[0], casts)
+        found = search_items(my_data, parent, proba)
+        print(my_data)
+        if found:
+            print(found)
+            my_data = cast(found, my_data)
+        else:
+            print('Error')
+        count += 1
+        if count >= 10:
+            print("timeout")
+            break
